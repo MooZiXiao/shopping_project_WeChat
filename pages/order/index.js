542fb1acb43362a87040fe19ff9181b37023c627
Zioo Moo <571869136@qq.com>
2019-09-29 16:59:32 +0800

    11.2订单页面结构渲染

--- pages/order/index.js
+++ pages/order/index.js
@@ -1,9 +1,17 @@
+// 引入
+import regeneratorRuntime from '../../lib/runtime/runtime.js';
+import {request} from '../../request/index.js'
 // pages/order/index.js
 Page({
   /**
    * 页面的初始数据
    */
   data: {
+    // 全部订单
+    allOrderData: [],
+    // 未付款订单
+    nonPayOrderData: [],
+    nonSendOrderData: [],
     // 自定义tab
     tabTitle: [
       { name: '全部'}, 
@@ -24,6 +32,38 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+    this.getOrderData()
+    this.getNonPayOrderData()
+    this.getNonSendOrderData()
+  },
+  /* 调用订单接口 */
+  async getOrderData(){
+    const {orders} = await request({
+      url: '/my/orders/all',
+      data: {type: 1}
+    })
+    this.setData({
+      allOrderData: orders
+    })
+  },
+  /* 调用订单接口 */
+  async getNonPayOrderData(){
+    const {orders} = await request({
+      url: '/my/orders/all',
+      data: {type: 2}
+    })
+    this.setData({
+      nonPayOrderData: orders
+    })
+  },
+  /* 调用订单接口 */
+  async getNonSendOrderData(){
+    const {orders} = await request({
+      url: '/my/orders/all',
+      data: {type: 3}
+    })
+    this.setData({
+      nonSendOrderData: orders
+    })
   }
 })
